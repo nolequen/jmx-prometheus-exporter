7ca1df2847df1e7302f9c732e164af9c1629fb6a
nolequen <nolequen@gmail.com>
2017-11-29 17:53:47 +0300

    fat agent jar #9

--- agent/src/main/java/su/nlq/prometheus/jmx/agent/CollectorAgent.java
+++ agent/src/main/java/su/nlq/prometheus/jmx/agent/CollectorAgent.java
@@ -1,38 +1,42 @@
 package su.nlq.prometheus.jmx.agent;
 
-import io.prometheus.client.hotspot.DefaultExports;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.kohsuke.args4j.Argument;
 import su.nlq.prometheus.jmx.arguments.Arguments;
 import su.nlq.prometheus.jmx.http.CollectorServer;
 import su.nlq.prometheus.jmx.http.ExpositionFormat;
 import su.nlq.prometheus.jmx.http.ServerParameters;
 
-import java.lang.instrument.Instrumentation;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public enum CollectorAgent {
   ;
 
-  public static void premain(@NotNull String argument, @NotNull Instrumentation instrumentation) {
-    Arguments.of(argument.split(":"), new AgentArguments()).ifPresent(arguments -> {
-      CollectorServer.start(arguments);
-      DefaultExports.initialize();
-    });
+  private static @NotNull AtomicBoolean launched = new AtomicBoolean();
+
+  public static void premain(@Nullable String arguments) {
+    if (launched.compareAndSet(false, true)) {
+      Arguments
+          .of(Optional.ofNullable(arguments).orElse("").split(":"), new AgentArguments())
+          .ifPresent(CollectorServer::start);
+    }
   }
 
   private static final class AgentArguments extends ServerParameters {
 
-    @Argument(index = 0, metaVar = "FORMAT", usage = "exposition format", required = false)
-    private @NotNull ExpositionFormat format = ExpositionFormat.Text;
+    @Argument(index = 0, metaVar = "CONFIG", usage = "configuration xml file (required)", required = true)
+    private @NotNull String config = "";
 
-    @Argument(index = 1, metaVar = "HOST", usage = "host", required = false)
-    private @NotNull String host = "";
-
-    @Argument(index = 2, metaVar = "PORT", usage = "port", required = true)
+    @Argument(index = 1, metaVar = "PORT", usage = "port (required)", required = true)
     private int port = 0;
 
-    @Argument(index = 3, metaVar = "FILE", usage = "configuration xml file", required = true)
-    private @NotNull String config = "";
+    @Argument(index = 2, metaVar = "HOST", usage = "host", required = false)
+    private @NotNull String host = "localhost";
+
+    @Argument(index = 3, metaVar = "FORMAT", usage = "exposition format", required = false)
+    private @NotNull ExpositionFormat format = ExpositionFormat.Text;
 
     @Override
     protected @NotNull String getConfig() {
